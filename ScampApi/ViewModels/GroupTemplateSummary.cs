e829182e5e9fcbfe6ac8987f0564211f39783851
Stuart Leeks <stuart@leeks.net>
2015-03-17 16:19:10 -0700

    Add GroupTemplatesController and GroupUsersController

---
+++ ScampApi/ViewModels/GroupTemplateSummary.cs
@@ -0,0 +1,9 @@
+﻿namespace ScampApi.ViewModels
+{
+    public class GroupTemplateSummary
+    {
+        public int GroupId { get; set; }
+        public int TemplateId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ScampApi/ViewModels/GroupUserSummary.cs
@@ -0,0 +1,9 @@
+﻿namespace ScampApi.ViewModels
+{
+    public class GroupUserSummary
+    {
+        public int GroupId { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+    }
+}
