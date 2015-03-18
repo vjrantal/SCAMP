e7cdb1af520cbf42a5782d45f07c1d5bc64deaf4
Brad Merrill <brad_merrill@hotmail.com>
2015-03-18 15:21:26 -0700

    move identity out of repos

--- Identity/Scripts/indexCtrl.js
+++
@@ -1,5 +0,0 @@
-﻿'use strict';
-angular.module('corsApp')
-.controller('indexCtrl', ['$scope', 'adalAuthenticationService', function ($scope, adalService) {
-
-}]);
--- Identity/Scripts/userDataCtrl.js
+++
@@ -1,6 +0,0 @@
-﻿'use strict';
-angular.module('corsApp')
-.controller('userDataCtrl', ['$scope', 'adalAuthenticationService', function ($scope, adalService) {
-
-
-}]);
