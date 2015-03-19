6ceb6df333cb4aa12faa92f3bbddf1003a30fd4e
Gabriele Castelani <gabrielecastellani@hotmail.com>
2015-03-18 23:21:56 -0700

    First alpha version of the provisioning system

---
+++ AzureProvisioningLibrary/QueueMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureProvisioningLibrary
+{
+    public class QueueMessage
+    {
+        public Guid OperationGuid { get; set; }
+        public int ResourceId { get; set; }
+        public ResourceAction Action { get; set; }
+    }
+}
