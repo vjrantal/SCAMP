eb63f9a253b0c13a05143d516bbacb3bfde3586b
Fed29 <federico.angaramo@outlook.com>
2015-04-10 15:39:05 +0200

    Fixed wrong beliefs on group members
    Fixed wrong beliefs on group members

--- ScampApi/wwwroot/App/Scripts/dashboardCtrl.js
+++ ScampApi/wwwroot/App/Scripts/dashboardCtrl.js
@@ -1,46 +1,46 @@
 'use strict';
 angular.module('scamp')
 .controller('dashboardCtrl', ['$scope', '$modal', '$location', 'dashboardSvc', 'groupsSvc', 'adalAuthenticationService', function ($scope, $modal, $location, dashboardSvc, groupsSvc, adalService) {
-    $scope.currentRouteName = 'Dashboard';
-    $scope.userList = null;
+	$scope.currentRouteName = 'Dashboard';
+	$scope.userList = null;
 
-    $scope.populate = function() {
-        console.log(dashboardSvc);
-        dashboardSvc.getItems().success(function (results) {
-            $scope.userList = results;
-            $scope.loadingMessage = "";
-        }).error(function (err) {
-            $scope.error = err;
-            $scope.loadingMessage = "";
-        })
+	$scope.populate = function () {
+		console.log(dashboardSvc);
+		dashboardSvc.getItems().success(function (results) {
+			$scope.userList = results;
+			$scope.loadingMessage = "";
+		}).error(function (err) {
+			$scope.error = err;
+			$scope.loadingMessage = "";
+		})
 
-        groupsSvc.getItems().success(function (results) {
-        	$scope.groupList = results;
-        	$scope.loadingMessage = "";
-        }).error(function (err) {
-            $scope.error = err;
-            $scope.loadingMessage = "";
-        });
-    };
+		groupsSvc.getItems().success(function (results) {
+			$scope.groupList = results;
+			$scope.loadingMessage = "";
+		}).error(function (err) {
+			$scope.error = err;
+			$scope.loadingMessage = "";
+		});
+	};
 
-    $scope.manageGroup = function (groupId) {
-    	var modalInstance = $modal.open({
-    		templateUrl: 'GroupUsers.html',
-    		controller: 'GroupUsersModalCtrl',
-    		size: 'lg',
-    		resolve: {
-    			groupSvc: function () {
-    				return groupsSvc;
-    			},
-    			group: function () {
-    				return groupsSvc.getItem(groupId);
-    			},
-    			users: function () {
-    				return $scope.userList;
-    			}
-    		}
-    	});
-    };
+	$scope.manageGroup = function (groupId) {
+		var modalInstance = $modal.open({
+			templateUrl: 'GroupUsers.html',
+			controller: 'GroupUsersModalCtrl',
+			size: 'lg',
+			resolve: {
+				groupSvc: function () {
+					return groupsSvc;
+				},
+				group: function () {
+					return groupsSvc.getItem(groupId);
+				},
+				users: function () {
+					return $scope.userList;
+				}
+			}
+		});
+	};
 }]);
 
 
@@ -59,14 +59,31 @@ angular.module('scamp')
 		$modalInstance.dismiss('done');
 	};
 
-	$scope.addUser = function (user, userRole) {
+	$scope.addAdmin = function (user) {
 		var newGroup = group.data;
-		if (userRole == "admin")
-			newGroup.admins.push(user);
-		else
-			newGroup.members.push(user);
+		newGroup.admins.push(user);
 		groupSvc.putItem(newGroup.groupId, newGroup);
 	};
-
-	$scope.userRole = "member";
+})
+.filter('notAdmin', function () {
+	return function (users, admins) {
+		var filtered = [];
+		var u, a;
+		var found = false;
+		for (u of users) {
+			if (!userInArray(u.userId, admins))
+				filtered.push(u);
+		}
+		return filtered;
+	};
 });
+
+function userInArray(userId, array) {
+	var u;
+	for (u of array) {
+		if (userId == u.userId) {
+			return true;
+		}
+	}
+	return false;
+}
