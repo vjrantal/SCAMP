105bf21db68f97fddf010b113f2aa552ccac3499
Brent Stineman <brent.stineman@microsoft.com>
2015-04-17 15:23:09 -0500

    Adding basic display for system admin setup

--- ScampApi/wwwroot/App/Scripts/settingsAdminsCtrl.js
+++ ScampApi/wwwroot/App/Scripts/settingsAdminsCtrl.js
@@ -3,6 +3,10 @@ angular.module('scamp')
 .controller('settingsAdminsCtrl', ['$scope', 'adalAuthenticationService', '$location', 'settingsAdminsSvc', function ($scope, adalService, $location, settingsAdminsSvc) {
     $scope.currentRouteName = 'settingsAdmins';
 
-
+    // gets a list of currently configuration subscription
+    $scope.getSystemAdmins = function () {
+        $scope.adminList = settingsAdminsSvc.getSystemAdmins();
+        console.log($scope.adminList);
+    };
 
 }]);
