4da1462b32c16778fe89f4bcb65f793ce6457311
khenidak <khnidk@outlook.com>
2015-05-06 15:46:50 -0700

    Activity Log Storage

---
+++ ProvisioningLibrary/VolatileStorage/VolatileStorageControllerExtentions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ProvisioningLibrary;
+
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+
+
+namespace ProvisioningLibrary
+{
+    public static class VolatileStorageExtentions
+    {
+        public static void UseVolatileStorage(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IVolatileStorageController, VolatileStorageController>();
+        }
+
+    }
+}
