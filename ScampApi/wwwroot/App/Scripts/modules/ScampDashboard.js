36183b09577d48d50b8c9b777367240126f48a9e
unknown <erisch@MININT-BBBU6VP.northamerica.corp.microsoft.com>
2015-05-27 16:04:52 -0400

    Fix dashboard summary panel issue

--- ScampApi/wwwroot/App/Scripts/modules/ScampDashboard.js
+++ ScampApi/wwwroot/App/Scripts/modules/ScampDashboard.js
@@ -56,8 +56,8 @@
     this.updateSummaryPanel = function(data){
         scope.summary = {
             'totUnitsUsed': data.totUnitsUsed,
-            'pctUsed': (data.totUnitsAllocated > 0) ? Math.round((user.totUnitsUsed / data.totUnitsAllocated) * 100) : 0,
-            'pctAllocated': (data.unitsBudgeted > 0) ? Math.round((user.totUnitsAllocated / data.unitsBudgeted) * 100) : 0,
+            'pctUsed': (data.totUnitsAllocated > 0) ? Math.round((data.totUnitsUsed / data.totUnitsAllocated) * 100) : 0,
+            'pctAllocated': (data.unitsBudgeted > 0) ? Math.round((data.totUnitsAllocated / data.unitsBudgeted) * 100) : 0,
             'budget': data.unitsBudgeted,
             'noGroups': data.totGroups,
             'totUnitsAllocated': data.totUnitsAllocated
