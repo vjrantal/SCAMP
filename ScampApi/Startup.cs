e77281748e2c8bbd7be39cc8bec105d88f3f57a4
Brent Stineman <brent.stineman@microsoft.com>
2015-05-27 15:55:24 -0500

    removed partial caching implementation, updated SSL port option related to issue #176

--- ScampApi/Startup.cs
+++ ScampApi/Startup.cs
@@ -60,7 +60,6 @@ namespace ScampApi
 
             services.AddDocumentDbRepositories(Configuration);
             services.AddKeyVaultRepositories(Configuration);
-            services.AddCacheProvider(Configuration);
         }
 
         // Configure is called after ConfigureServices is called.
